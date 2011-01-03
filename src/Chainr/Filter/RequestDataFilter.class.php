fee8a196af75f7cbc9e5025230e4f26773912c90
Christian Schmitz <csc@soulworks.de>
2011-01-03 15:18:55 +0100

    Tidy up

--- src/Chainr/Filter/RequestDataFilter.class.php
+++ src/Chainr/Filter/RequestDataFilter.class.php
@@ -1,6 +1,20 @@
 <?php
 
 /**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * This filter appends all get- and cookie-parameters to the site's source.
  * 
  * @package Chainr
@@ -24,6 +38,7 @@ class Chainr_Filter_RequestDataFilter extends Chainr_Filter implements Chainr_In
 				$ps->appendTextNode('parameter', $v)->appendAttribute('name', $k);
 			}
 		}
+
 		if (is_array($_COOKIE) && count($_COOKIE) > 0) {
 			$ps = $node->appendNode('parameters')->appendAttribute('method', 'cookie');
 			foreach($_COOKIE as $k => $v) {
--- src/Chainr/Filter/RoutingFilter.class.php
+++ src/Chainr/Filter/RoutingFilter.class.php
@@ -1,6 +1,20 @@
 <?php
 
 /**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * @package Chainr
  * @subpackage Filter
  * 
@@ -53,7 +67,7 @@ class Chainr_Filter_RoutingFilter extends Chainr_Filter implements Chainr_InputF
 		$segmentsNode = $node->appendNode('segments');
 		foreach ($segments as $k => $v) {
 			$segmentsNode->appendTextNode('segment', $v)
-					       ->appendAttribute('id', $k);
+				         ->appendAttribute('id', $k);
 		}
 
 		$node->appendTextNode('page', $segments[0]);
